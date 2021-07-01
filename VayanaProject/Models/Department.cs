e75b7ead1808a1b27f5a40813558f572bfafd059
Ankush <manaktalaankush9@gmailcom>
2021-07-01 20:43:09 +0530

    Add project files.

---
+++ VayanaProject/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VayanaProject.Models
+{
+    public class Department
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+        public string DeptCode { get; set; }
+    }
+}
